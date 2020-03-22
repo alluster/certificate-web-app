8bf8345d30b05cfe23830d850f2b003df20826e8
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-03-22 14:48:58 +0200

    user registration added

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,14 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-             <Auth0Provider
-                domain={`${process.env.AUTHO_DOMAIN}`}
-                clientId={`${process.env.AUTHO_CLIENT_ID}`}
-                redirectUri={`${process.env.AUTHO_REDIRECT_URI}`}
-			>
-                <Provider>
-
-					<Component {...pageProps} />
-            	</Provider>
-
-            </Auth0Provider>
+		<Auth0Provider
+			domain={JSON.stringify(process.env.AUTHO_DOMAIN)}
+			clientId={JSON.stringify(process.env.AUTHO_CLIENT_ID)}
+			redirectUri={JSON.stringify(process.env.AUTHO_REDIRECT_URI)}>
+			<Provider>
+				<Component {...pageProps} />
+			</Provider>
+		</Auth0Provider>
     );
   }
 }

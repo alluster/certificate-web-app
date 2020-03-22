74ccf98ff7cc2afabbf833cdd45e08859165e84f
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-03-22 14:31:34 +0200

    user registration added

--- components/TopNavigation/index.js
+++ components/TopNavigation/index.js
@@ -84,7 +84,7 @@ const TopNavigation = ({ className }) => {
               </>
             ) : (
               <Gx col={2}>
-                <LinkText onClick={() => login({ appState: { returnTo: process.env.AUTHO_REDIRECT_URI + '/profile' } })}>
+                <LinkText onClick={() => login({ appState: { returnTo: process.env.DOMAIN + '/profile' } })}>
                   Log in
                 </LinkText>
               </Gx>
